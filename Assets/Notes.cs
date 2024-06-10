9582153f76af121786d87359c4cb0b0a93dc39c5
CIIT601PC01\Student <it@ciit.edu.ph>
2024-06-10 10:44:20 +0800

    Added notes script for notes

---
+++ Assets/Notes.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+//Notes:
+//Finals will be VFs
+//BFS is basically mechanics ng Neurons
+//    nodes thingy
