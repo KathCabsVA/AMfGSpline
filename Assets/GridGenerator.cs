3468a6e4e2a63444a2ad79dccffc1755e84b5405
CIIT601PC01\Student <it@ciit.edu.ph>
2024-06-10 09:47:54 +0800

    Created GridGenerator Script and attached it to an empty game object

---
+++ Assets/GridGenerator.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+    public int width, height;
+    public GameObject tilePrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateGrid()
+    {
+        if (tilePrefab == null)
+        {
+            Debug.LogError("No Prefab Assigned");
+            return;
+        }
+        //Loop through the grid positions
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                //calculate the position for each cube
+                Vector3 position = new Vector3(x, 0, y);
+                //Instantiate the cube at the calculated position
+                GameObject newTile = Instantiate(tilePrefab, position, Quaternion.identity);
+                newTile.transform.parent = transform;
+                newTile.tag = "Tile";
+
+            }
+        }
+    }
+
+}
