245f6436de88a2078aa8866f6827b9f961e32ae1
CIIT601PC01\Student <it@ciit.edu.ph>
2024-06-10 09:56:55 +0800

    added GridGeneratorEditor script in order to instantiate the cubes without playing the start button

--- Assets/GridGenerator.cs
+++ Assets/GridGenerator.cs
@@ -11,7 +11,7 @@ public class GridGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GenerateGrid();
+        //GenerateGrid();
     }
 
     // Update is called once per frame
